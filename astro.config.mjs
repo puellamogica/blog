c0639427c02bebe606204e9fb8d8a04253989b02
Amia33 <137411471+Amia33@users.noreply.github.com>
2026-09-11 09:21:18 +0800

    Add Astro blog scaffold for Cloudflare Workers

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://20190716.xyz",
+  adapter: cloudflare(),
+});
